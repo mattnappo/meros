32541e7eae0a3eba82ab33c7557ea9c1477500a0
xoreo <mattnappo@gmail.com>
2019-04-15 22:07:04 -0400

    core: Writing unit tests.

--- core/split_test.go
+++ core/split_test.go
@@ -2,11 +2,23 @@ package core
 
 import "testing"
 
+func TestCalculateSizeSum(t *testing.T) {
+	sizeVector := []uint32{1, 2, 3, 4} // Sum = 10
+	sum := calculateSizeSum(sizeVector)
+	t.Logf("sum: %d\n", sum)
+
+	sizeVector = []uint32{25, 13, 342, 92, 12, 456, 2} // Sum = 942
+	sum = calculateSizeSum(sizeVector)
+	t.Logf("sum: %d\n", sum)
+}
+
 func TestSplitBytes(t *testing.T) {
 	bytes := []byte("1234")
 	sizes := []uint32{1, 2, 2}
-}
 
-func TestcalculateSizeSum(t *testing.T) {
-
+	splitBytes, err := SplitBytes(bytes, sizes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(splitBytes)
 }
