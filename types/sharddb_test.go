d6f8917cfd382d725c02b006c950d0bfaad10117
xoreo <mattnappo@gmail.com>
2020-03-11 18:14:37 -0400

    types: DB tests passed.

--- types/sharddb_test.go
+++ types/sharddb_test.go
@@ -8,7 +8,9 @@ import (
 )
 
 func TestGenerateShardDB(t *testing.T) {
-	shards, err := GenerateShards([]byte("im a neat test file"), 3)
+	// Generate shards
+	testBytes := []byte("im a neat test file")
+	shards, err := GenerateShards(testBytes, 3)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -25,23 +27,30 @@ func TestGenerateShardDB(t *testing.T) {
 		nodes = append(nodes, *randomNode)
 	}
 
+	// Generate shardDB
 	sharddb, err := generateShardDB(shards, nodes)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// Test the map
 	for k, v := range sharddb.shardMap {
 		t.Logf("%x: %v\n\n", k, v)
 	}
 
+	// Test map access
 	h, err := crypto.HashFromString("bafc4c93a862aecc87368f291090fc2fe479eecc3bd15b7efdde01ff92c42592")
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Logf("\nGET: [%v]\n", sharddb.shardMap[shardID(h)])
 
+	// Analyze shard sizes
+	var sizeCounter uint32
 	for _, v := range sharddb.shardMap {
 		t.Logf("size: %d\n", v.Size)
+		sizeCounter = sizeCounter + v.Size
 	}
-
+	t.Logf("correct size: %d", len(testBytes))
+	t.Logf(" actual size: %d", sizeCounter)
 }
