84f2ab140cbb24205eeca7862e43e1dce932c8b1
xoreo <mattnappo@gmail.com>
2020-03-13 23:48:50 -0400

    filedb: Wrote fileDB models.

--- filedb/filedbmodel.go
+++ filedb/filedbmodel.go
@@ -1,1 +1,14 @@
 package filedb
+
+import (
+	"github.com/xoreo/meros/crypto"
+	"github.com/xoreo/meros/types"
+)
+
+// fileID is the model for keys in the file database.
+type fileID crypto.Hash
+
+// generateFileEntry generates a fileID-file pair for the fileDB.
+func generateFileEntry(file types.File) (fileID, types.File) {
+	return fileID(file.Hash), file
+}
