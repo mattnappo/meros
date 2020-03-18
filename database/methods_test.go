7b85739c12b05efe71fc1fadfdb471f33877f047
xoreo <mattnappo@gmail.com>
2020-03-17 23:06:19 -0400

    database: Fixed unit tests.

--- database/methods_test.go
+++ database/methods_test.go
@@ -27,25 +27,37 @@ func TestPutFile(t *testing.T) {
 }
 
 func TestGetFile(t *testing.T) {
+
+	/* -- PUT -- */
+
 	filedb, err := Open("myfiledb", FILEDB)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer filedb.Close()
 
-	fileid, err := IDFromString("8ade293f4d76ba0e0b1101c1274dda59925ecb178b763c12126a70d54d1b5469")
+	file, err := types.NewFile("test_file.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	file, err := filedb.GetItem(fileid)
+	fid, err := filedb.PutItem(*file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("fileID: %s\n", fid.String())
+
+	/* -- GET -- */
+
+	readFile, err := filedb.GetItem(fid)
 	if err != nil {
 		t.Logf("%v", file)
 		t.Fatal(err)
 	}
 
-	v := file.(*types.File)
-	t.Logf("file '%s': %v\n", fileid.String(), v)
+	v := readFile.(*types.File)
+	t.Logf("file '%s': %v\n", fid.String(), v)
 }
 
 func TestPutShard(t *testing.T) {
